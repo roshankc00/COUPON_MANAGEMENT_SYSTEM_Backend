a681b2ec612d19e1e52e08ed41ab60f748fc45fa
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-05-23 00:58:02 +0545

    fix build

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -3,6 +3,66 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { datasourceOptions } from 'typeorm.config';
 
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { AffiliateLink } from 'src/affiliate-link/entities/affiliate-link.entity';
+import { BlogItem } from 'src/blogs/entities/blog-item.entity';
+import { Blog } from 'src/blogs/entities/blog.entity';
+import { Cashback } from 'src/cashback/entities/cashback.entity';
+import { Category } from 'src/category/entities/category.entity';
+import { Seo } from 'src/common/entity/Seo.entity';
+import { Coupon } from 'src/coupons/entities/coupon.entity';
+import { Faq } from 'src/faqs/entities/faq.entity';
+import { Feedback } from 'src/feedback/entities/feedback.entity';
+import { Follower } from 'src/followers/entities/follower.entity';
+import { Purchase } from 'src/purchase/entities/purchase.entity';
+import { Review } from 'src/review/entities/review.entity';
+import { Store } from 'src/store/entities/store.entity';
+import { SubCategory } from 'src/sub-categories/entities/sub-category.entity';
+import { SubmitOffer } from 'src/submit-offer/entities/submit-offer.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Wishlist } from 'src/wishlists/entities/wishlist.entity';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import fs from 'fs-extra';
+
+config();
+
+const configService = new ConfigService();
+
+const datasourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.getOrThrow('POSTGRES_HOST'),
+  port: configService.getOrThrow('POSTGRES_PORT'),
+  username: configService.getOrThrow('POSTGRES_USERNAME'),
+  database: configService.getOrThrow('POSTGRES_DATABASE'),
+  password: configService.getOrThrow('POSTGRES_PASSWORD'),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: [
+    User,
+    Coupon,
+    Category,
+    Seo,
+    Store,
+    SubCategory,
+    Faq,
+    Wishlist,
+    Follower,
+    Feedback,
+    Review,
+    Cashback,
+    Purchase,
+    AffiliateLink,
+    SubmitOffer,
+    Blog,
+    BlogItem,
+  ],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  synchronize: false,
+  subscribers: [],
+};
+
 @Module({
   imports: [TypeOrmModule.forRoot(datasourceOptions)],
 })
